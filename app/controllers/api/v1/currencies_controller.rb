0c83c5064af18cc5071659259066ea7e07b3afe4
extazystas <extazystas@gmail.com>
2020-10-10 15:37:31 +0300

    Fix rubocop codestyle offences

--- app/controllers/api/v1/currencies_controller.rb
+++ app/controllers/api/v1/currencies_controller.rb
@@ -4,11 +4,12 @@ module Api
       def index
         requested_day = params[:day]
 
-        currencies = if requested_day.present?
-          Array(Currency.where(valid_at: Time.parse(requested_day).to_date))
-        else
-          Currency.all
-        end
+        currencies =
+          if requested_day.present?
+            Array(Currency.where(valid_at: Time.parse(requested_day).to_date))
+          else
+            Currency.all
+          end
 
         render json: currencies, each_serializer: Api::V1::CurrencySerializer, status: :ok
       end
--- app/serializers/api/v1/currency_serializer.rb
+++ app/serializers/api/v1/currency_serializer.rb
@@ -1,9 +1,11 @@
-module Api::V1
-  class CurrencySerializer < ActiveModel::Serializer
-    attributes :id, :valid_at, :daily_rates
+module Api
+  module V1
+    class CurrencySerializer < ActiveModel::Serializer
+      attributes :id, :valid_at, :daily_rates
 
-    def valid_at
-      object.valid_at.strftime('%Y-%m-%d')
+      def valid_at
+        object.valid_at.strftime('%Y-%m-%d')
+      end
     end
   end
 end
