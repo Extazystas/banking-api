b2d0c3242ccf7531f2005ce0836accc26d99fb07
extazystas <extazystas@gmail.com>
2020-10-10 14:30:44 +0300

    Set DB schema format to SQL

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,9 @@ module BankingApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.active_record.schema_format = :sql
+
+    config.eager_load_paths += %W(#{config.root}/app/**/*.rb #{config.root}/lib)
   end
 end
